94f368d8126b590de28b2909cecd79a04401b6b4
Kristiyan Halachev <kristianhalachev0@gmail.com>
2025-12-10 12:05:40 +0200

    OOps forgot the deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Pulling latest code..."
+cd /var/www/tripTuk-Next.js
+
+# force sync with GitHub
+git fetch origin main
+git reset --hard origin/main
+git clean -fd
+
+echo "Installing dependencies..."
+npm install
+
+echo "Building frontend..."
+npm run build
+
+echo "Restarting server..."
+pm2 restart dAudio.dev
+
+echo "Deplyment complete"
